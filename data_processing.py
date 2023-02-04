62f33b98a5edefba03e9f80d89c6faf255e9ead2
jn2109 <nigam21jyoti@gmail.com>
2023-02-04 16:31:20 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -3,3 +3,12 @@ print("This file data processing code for fraud detection")
 print("test")
 
 
+
+a=5
+b=6
+
+def sum(a,b):
+    c=a+b
+    return(a)
+
+print("added a new function sum which is not correct")
